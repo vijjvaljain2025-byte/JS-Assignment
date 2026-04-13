3d4abb0f43fe65e6355a5c776b95667c3c7e3264
Vijjval Jain <vijjval.jain2025@students.sau.ac.in>
2026-04-13 14:15:21 +0530

    Assignment Q2

---
+++ q2.js
@@ -0,0 +1,23 @@
+// Q2: Roll-Seed Lock
+
+let N = parseInt(prompt("Enter N:"));
+let seed = parseInt(prompt("Enter seed:"));
+
+let current = N;
+
+for (let i = 0; i < 3; i++) {
+    if (current % 2 === 0)
+        current = Math.floor(current / 2) + seed;
+    else
+        current = current * 3 - seed;
+}
+
+let result = "NO";
+
+if (current >= 100 && current <= 999) {
+    let middle = Math.floor(current / 10) % 10;
+    if (middle === seed)
+        result = "YES";
+}
+
+alert(result + ", " + current);
